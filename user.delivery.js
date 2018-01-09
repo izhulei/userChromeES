91f268edfd92c10be57546a661d3e4f0fe84dc7e
izhulei <izhulei@163.com>
2018-01-09 11:30:24 +0800

    Update user.delivery.js

--- user.delivery.js
+++ user.delivery.js
@@ -2,7 +2,6 @@
 // @name         微店导入发货
 // @namespace    https://github.com/izhulei/userjs
 // @version      0.2
-// @updateURL    https://raw.githubusercontent.com/izhulei/userjs/master/user.delivery.js
 // @description  https://github.com/knrz/CSV.js使用了CSV处理js
 // @author       zhulei
 // @match        http://10522mcm.web08.com.cn/OrderForm/NewOrderList*
